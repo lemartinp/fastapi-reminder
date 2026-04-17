0af4b3dc744bdccaaa92147e5bb06fe319591260
lemartinp <lemartinp@unal.edu.co>
2026-04-17 15:47:48 -0500

    added some endpoints

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from models import Product
 
-
 app = FastAPI()
 
 @app.get("/")
@@ -17,3 +16,25 @@ products = [
 @app.get("/products")
 def get_all_products():
     return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+        
+    return "product not found"
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+@app.put("/product")
+def update_product(id: int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "Product Added Successfully"
+        
+    return "Product not found"
